c14567fe970a8ef99e84b0ffa7bcce7881319c6e
Luis Olivas <olivasl@berea.edu>
2026-02-17 21:09:27 -0500

    [853] [Time Beats: 81.16%] [Memory Beats: 92.14%] - LeetPush

--- Car-Fleet.py
+++ Car-Fleet.py
@@ -1,11 +1,11 @@
 1class Solution:
 2    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-3        pairs = [(p,s) for p,s in zip(position, speed)]
-4        stack = []
+3        res = []
+4        pairs = [(p,s) for p,s in zip(position, speed)]
 5
 6        for p, s in sorted(pairs)[::-1]:
-7            stack.append((target - p) / s)
-8            if len(stack) >= 2 and stack[-1] <= stack[-2]:
-9                stack.pop()
+7            res.append((target - p) / s)
+8            if len(res) >= 2 and res[-2] >= res[-1]:
+9                res.pop()
 10
-11        return len(stack)
+11        return len(res)
