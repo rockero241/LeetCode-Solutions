e19832b3345c65a8d3934eeaed399a654868b7aa
Luis Olivas <olivasl@berea.edu>
2026-03-02 16:39:54 -0500

    File updated at 3/2/2026, 4:39:54 PM

--- 704-BinarySearch/704-BinarySearch.py
+++ 704-BinarySearch/704-BinarySearch.py
@@ -1,9 +1,9 @@
-# Last updated: 3/2/2026, 4:39:24 PM
+# Last updated: 3/2/2026, 4:39:54 PM
 1class Solution:
 2    def search(self, nums: List[int], target: int) -> int:
 3        l, r = 0, len(nums) - 1
 4        while l <= r:
-5            m = (l + r) // 2
+5            m = (l+r) // 2
 6            if nums[m] < target:
 7                l = m + 1
 8            elif nums[m] > target:
