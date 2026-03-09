96919e15804d142a697192b79e3f90b3b43968ee
Luis Olivas <olivasl@berea.edu>
2026-03-09 06:37:34 -0500

    File updated at 3/9/2026, 7:37:34 AM

--- 853-CarFleet/853-CarFleet.py
+++ 853-CarFleet/853-CarFleet.py
@@ -1,12 +1,12 @@
-# Last updated: 3/6/2026, 9:52:20 PM
+# Last updated: 3/9/2026, 7:37:34 AM
 1class Solution:
 2    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-3        pairs = [[p, s] for p,s in zip(position, speed)]
+3        pairs = [[p,s] for p,s in zip(position, speed)]
 4        res = []
 5
 6        for p, s in sorted(pairs)[::-1]:
 7            res.append((target - p) / s)
-8            if len(res) >= 2 and res[-2] >= res[-1]:
+8            if len(res) >= 2 and res[-1] <= res[-2]:
 9                res.pop()
-10        
+10
 11        return len(res)
